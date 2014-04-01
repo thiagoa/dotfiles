bd997b76d857abdaa4ebe84be99f85eed7bf04c9
Thiago A. Silva <thiago@mixinternet.com.br>
2014-04-01 13:36:10 -0300

    Add common file for aliases

---
+++ aliases.sh
@@ -0,0 +1,13 @@
+# Source this file in bash, ZSH or any sh compatible shell
+
+# See currently opened network connections
+alias connections="lsof -P -i -n"
+
+# See external IP
+alias myip="curl ifconfig.me"
+
+# Launch PostgreSQL
+alias pg="postgres -D /usr/local/var/postgres"
+
+# Launch MySQL
+alias mysql="mysql -u root --password="
