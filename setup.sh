12548763e0d4fbfc245081312c84b90c42c9a2a9
Thiago A. Silva <thiagoaraujos@gmail.com>
2017-04-24 01:20:16 -0300

    Make git commands in setup file less verbose

--- setup.sh
+++ setup.sh
@@ -31,7 +31,7 @@ function install_binfiles  {
 
     echo "Installing bin directory...\n"
 
-    git clone --quiet https://github.com/thiagoa/bin $HOME/bin
+    git clone -q https://github.com/thiagoa/bin $HOME/bin
 }
 
 function install_asdf {
@@ -40,7 +40,7 @@ function install_asdf {
     echo "Installing asdf...\n"
 
     # Still need to figure out how to get the most up-to-date version...
-    git clone --quiet https://github.com/asdf-vm/asdf.git $HOME/.asdf --branch v0.3.0 > /dev/null
+    git clone -q https://github.com/asdf-vm/asdf.git $HOME/.asdf --branch v0.3.0 > /dev/null
 }
 
 function generate_ssh_key {
@@ -58,7 +58,7 @@ function install_zprezto {
 
     echo "Installing zprezto..."
 
-    git clone --quiet --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto" > /dev/null 2>&1
+    git clone -q --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto" > /dev/null 2>&1
 
     for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
         ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
@@ -82,7 +82,7 @@ function install_vimfiles {
 
     echo "Installing vimfiles...\n"
 
-    git clone --quiet https://github.com/thiagoa/dotvim $HOME/.vim
+    git clone -q https://github.com/thiagoa/dotvim $HOME/.vim
 
     $HOME/.vim/setup.sh
 }
@@ -93,7 +93,7 @@ function install_base16 {
 
     echo "\nInstalling base16 themes..."
 
-    git clone --quiet https://github.com/chriskempson/base16-shell.git $HOME/.config/base16-shell
+    git clone -q https://github.com/chriskempson/base16-shell.git $HOME/.config/base16-shell
 
     BASE16_SHELL=$HOME/.config/base16-shell/
     eval "$($BASE16_SHELL/profile_helper.sh)"
