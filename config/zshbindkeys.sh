09ff50e1304af8377d44f028e2757702e4a9213d
Thiago A. Silva <thiagoaraujos@gmail.com>
2017-10-24 23:37:51 -0300

    Alternative keybindings for use in neovim
    C-k and C-h are already occupied by commands to move to windows

--- config/zshbindkeys.sh
+++ config/zshbindkeys.sh
@@ -3,3 +3,5 @@ bindkey "\e," _history-complete-newer
 bindkey "^X1" after-first-word
 bindkey '^x^k' kill-region
 bindkey '^Z' fancy-ctrl-z
+bindkey '^]' kill-line
+bindkey '^G' backward-delete-char
