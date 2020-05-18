e2b95baa0889f38f5f66b4464b88985a33244462
Thiago A. Silva <thiagoaraujos@gmail.com>
2020-05-18 12:59:52 -0300

    Fix paths

--- linux/gnome-shortcuts/setup.sh
+++ linux/gnome-shortcuts/setup.sh
@@ -2,7 +2,7 @@
 
 [ ! -d "$HOME/.local" ] && exit
 
-source_dir=$HOME/.dotfiles/gnome-shortcuts
+source_dir=$HOME/.dotfiles/linux/gnome-shortcuts
 
 mkdir -p $HOME/.local/share/applications 2> /dev/null
 sudo ln -fs $source_dir/*.desktop $HOME/.local/share/applications/
