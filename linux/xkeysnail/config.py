8b32df7599e4e40f87051be3e0b369ab7e983642
Thiago A. Silva <thiagoaraujos@gmail.com>
2020-05-20 13:34:36 -0300

    Update xkeysnail config to the fact that Discord is a Chrome app (more power)

--- linux/xkeysnail/config.py
+++ linux/xkeysnail/config.py
@@ -27,7 +27,7 @@ define_multipurpose_modmap({
 
 # Must be before Firefox|Google-chrome|Slack|etc for some keybindings
 # to take precedence over the ones in there.
-define_keymap(re.compile("discord|Slack"), {
+define_keymap(re.compile("Discord|Slack"), {
     K("C-M-k"): K("C-k"),
     K("M-Shift-n"): K("M-Shift-down"),
     K("M-Shift-p"): K("M-Shift-up"),
@@ -35,7 +35,7 @@ define_keymap(re.compile("discord|Slack"), {
     K("C-Shift-p"): K("M-up")
 }, "Discord and Slack")
 
-define_keymap(re.compile("Firefox|Google-chrome|Slack|Thunderbird"), {
+define_keymap(re.compile("Firefox|Google-chrome|Slack|Discord|Thunderbird"), {
     K("C-M-n"): K("C-n"),
     K("M-p"): K("up"),
     K("M-n"): K("down"),
@@ -70,7 +70,7 @@ define_keymap(re.compile("Geary"), {
     K("C-M-n"): K("C-n"),
 }, "Geary")
 
-define_keymap(re.compile("discord|Slack"), {
+define_keymap(re.compile("Discord|Slack"), {
     K("C-M-k"): K("C-k"),
     K("M-Shift-n"): K("M-Shift-down"),
     K("M-Shift-p"): K("M-Shift-up"),
