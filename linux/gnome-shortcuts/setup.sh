f3b33a4f1fa5862ed65b81b5642a1c0a4e09f590
Thiago A. Silva <thiagoaraujos@gmail.com>
2020-06-14 22:15:39 -0300

    Setup emacsclient

--- linux/gnome-shortcuts/setup.sh
+++ linux/gnome-shortcuts/setup.sh
@@ -6,3 +6,6 @@ source_dir=$HOME/.dotfiles/linux/gnome-shortcuts
 
 mkdir -p $HOME/.local/share/applications 2> /dev/null
 sudo ln -fs $source_dir/*.desktop $HOME/.local/share/applications/
+
+# Register emacsclient shortcut
+xdg-mime default emacsclient.desktop text/plain
