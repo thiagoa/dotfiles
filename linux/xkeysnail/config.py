e2a5f1c21f440c62de94bad1ee276c461d2a41e3
Thiago A. Silva <thiagoaraujos@gmail.com>
2020-07-09 10:38:12 -0300

    Rearrange xkeysnail shortcuts order again :(

--- linux/xkeysnail/config.py
+++ linux/xkeysnail/config.py
@@ -34,84 +34,15 @@ define_multipurpose_modmap({
 this_config_file = sys.argv[-1] # __file__ doesn't work for this...
 ignored_apps_on_default_mappings = (Path(this_config_file).parent / 'ignored_apps_on_default_mappings').read_text()
 
-
-#### BEGIN GLOBAL EMACS
-#
-# For every global keybinding with a Control, map another global
-# keybinding that adds a Meta key to the former global keybinding. For
-# example, C-b is taken by the Emacs-like keybinding but we also map
-# C-M-b to C-b so that any potential shortcuts are are always
-# available throughout apps. No need to worry about Alt (Meta).
-
-define_keymap(lambda wm_class: wm_class not in ignored_apps_on_default_mappings.split('|'), {
-    K("C-b"): with_mark(K("left")),
-    K("C-M-b"): K("C-b"),
-    K("C-f"): with_mark(K("right")),
-    K("C-M-f"): K("C-f"),
-    K("C-p"): with_mark(K("up")),
-    K("C-M-p"): K("C-p"),
-    K("C-n"): with_mark(K("down")),
-    K("C-M-n"): K("C-n"),
-    K("C-Shift-b"): [K("left"), K("left"), K("left"), K("left"), K("left")],
-    K("C-Shift-f"): [K("right"), K("right"), K("right"), K("right"), K("right")],
-    K("C-Shift-n"): [K("down"), K("down"), K("down"), K("down"), K("down")],
-    K("C-Shift-p"): [K("up"), K("up"), K("up"), K("up"), K("up")],
-    K("M-b"): with_mark(K("C-left")),
-    K("M-f"): with_mark(K("C-right")),
-    K("C-a"): with_mark(K("home")),
-    K("C-M-a"): K("C-a"),
-    K("C-e"): with_mark(K("end")),
-    K("C-M-e"): K("C-e"),
-    K("M-v"): with_mark(K("page_up")),
-    K("C-v"): with_mark(K("page_down")),
-    K("C-M-v"): K("C-v"),
-    K("M-Shift-comma"): with_mark(K("C-home")),
-    K("M-Shift-dot"): with_mark(K("C-end")),
-    K("C-m"): K("enter"),
-    K("C-M-m"): K("C-m"),
-    K("C-o"): [K("enter"), K("left")],
-    K("C-M-o"): K("C-o"),
-    K("C-w"): [K("C-x"), set_mark(False)],
-    K("C-M-w"): K("C-w"),
-    K("M-w"): [K("C-c"), set_mark(False)],
-    K("C-y"): [K("C-v"), set_mark(False)],
-    K("C-M-y"): K("C-y"),
-    K("C-d"): [K("delete"), set_mark(False)],
-    K("C-M-d"): K("C-d"),
-    K("M-d"): [K("C-delete"), set_mark(False)],
-    K("M-backspace"): K("C-backspace"),
-    K("C-backspace"): [K("Shift-home"), K("delete"), set_mark(False)],
-    K("C-Shift-backspace"): [K("end"), K("Shift-home"), K("delete"), set_mark(False)],
-    K("C-k"): [K("Shift-end"), K("delete"), set_mark(False)],
-    K("C-M-k"): K("C-k"),
-    K("C-slash"): [K("C-z"), set_mark(False)],
-    K("C-Shift-slash"): [K("C-Shift-z"), set_mark(False)],
-    K("C-Shift-ro"): K("C-z"),
-    K("C-space"): toggle_mark(),
-    K("C-M-space"): with_or_set_mark(K("C-right")),
-    K("C-s"): K("F3"),
-    K("C-M-s"): K("C-s"),
-    K("C-r"): K("Shift-F3"),
-    K("C-r"): K("C-M-r"),
-    K("M-Shift-key_5"): K("C-h"),
-    K("C-g"): [K("esc"), set_mark(False), K("C")],
-    K("C-M-g"): K("C-g"),
-    K("C-q"): escape_next_key,
-    K("C-M-q"): K("C-q"),
-    K("C-x"): {
-        K("h"): [K("C-home"), K("C-a"), set_mark(True)],
-        K("C-f"): K("C-o"),
-        K("C-s"): K("C-s"),
-        K("k"): K("C-f4"),
-        K("C-c"): K("C-q"),
-        K("C-g"): pass_through_key,
-        K("u"): [K("C-z"), set_mark(False)],
-    },
-    K("C-M-s"): K("C-s")
-}, "Emacs-like keys")
-
-#### END GLOBAL EMACS
-
+define_keymap(re.compile("Org.gnome.Nautilus"), {
+    K("C-M-h"): K("M-home"),
+    K("C-M-p"): K("M-up"),
+    K("C-M-n"): K("M-down"),
+    K("C-M-l"): K("C-M-Win-l"),
+    K("M-Shift-n"): K("C-Shift-n"),
+    K("C-M-LEFT_BRACE"): K("C-page_up"),
+    K("C-M-RIGHT_BRACE"): K("C-page_down")
+}, "Chrome")
 
 #### BEGIN CHROME APPS
 #
@@ -205,17 +136,85 @@ define_keymap(re.compile("jetbrains-datagrip"), {
     K("C-M-LEFT_BRACE"): K("M-left"),
 }, "Datagrip")
 
-define_keymap(re.compile("Org.gnome.Nautilus"), {
-    K("C-M-h"): K("M-home"),
-    K("C-M-p"): K("M-up"),
-    K("C-M-n"): K("M-down"),
-    K("C-M-l"): K("C-M-Win-l"),
-    K("M-Shift-n"): K("C-Shift-n"),
-    K("C-M-LEFT_BRACE"): K("C-page_up"),
-    K("C-M-RIGHT_BRACE"): K("C-page_down")
-}, "Chrome")
-
 #### END ALL OTHER APPS
+
+
+#### BEGIN GLOBAL EMACS
+#
+# For every global keybinding with a Control, map another global
+# keybinding that adds a Meta key to the former global keybinding. For
+# example, C-b is taken by the Emacs-like keybinding but we also map
+# C-M-b to C-b so that any potential shortcuts are are always
+# available throughout apps. No need to worry about Alt (Meta).
+
+define_keymap(lambda wm_class: wm_class not in ignored_apps_on_default_mappings.split('|'), {
+    K("C-b"): with_mark(K("left")),
+    K("C-M-b"): K("C-b"),
+    K("C-f"): with_mark(K("right")),
+    K("C-M-f"): K("C-f"),
+    K("C-p"): with_mark(K("up")),
+    K("C-M-p"): K("C-p"),
+    K("C-n"): with_mark(K("down")),
+    K("C-M-n"): K("C-n"),
+    K("C-Shift-b"): [K("left"), K("left"), K("left"), K("left"), K("left")],
+    K("C-Shift-f"): [K("right"), K("right"), K("right"), K("right"), K("right")],
+    K("C-Shift-n"): [K("down"), K("down"), K("down"), K("down"), K("down")],
+    K("C-Shift-p"): [K("up"), K("up"), K("up"), K("up"), K("up")],
+    K("M-b"): with_mark(K("C-left")),
+    K("M-f"): with_mark(K("C-right")),
+    K("C-a"): with_mark(K("home")),
+    K("C-M-a"): K("C-a"),
+    K("C-e"): with_mark(K("end")),
+    K("C-M-e"): K("C-e"),
+    K("M-v"): with_mark(K("page_up")),
+    K("C-v"): with_mark(K("page_down")),
+    K("C-M-v"): K("C-v"),
+    K("M-Shift-comma"): with_mark(K("C-home")),
+    K("M-Shift-dot"): with_mark(K("C-end")),
+    K("C-m"): K("enter"),
+    K("C-M-m"): K("C-m"),
+    K("C-o"): [K("enter"), K("left")],
+    K("C-M-o"): K("C-o"),
+    K("C-w"): [K("C-x"), set_mark(False)],
+    K("C-M-w"): K("C-w"),
+    K("M-w"): [K("C-c"), set_mark(False)],
+    K("C-y"): [K("C-v"), set_mark(False)],
+    K("C-M-y"): K("C-y"),
+    K("C-d"): [K("delete"), set_mark(False)],
+    K("C-M-d"): K("C-d"),
+    K("M-d"): [K("C-delete"), set_mark(False)],
+    K("M-backspace"): K("C-backspace"),
+    K("C-backspace"): [K("Shift-home"), K("delete"), set_mark(False)],
+    K("C-Shift-backspace"): [K("end"), K("Shift-home"), K("delete"), set_mark(False)],
+    K("C-k"): [K("Shift-end"), K("delete"), set_mark(False)],
+    K("C-M-k"): K("C-k"),
+    K("C-slash"): [K("C-z"), set_mark(False)],
+    K("C-Shift-slash"): [K("C-Shift-z"), set_mark(False)],
+    K("C-Shift-ro"): K("C-z"),
+    K("C-space"): toggle_mark(),
+    K("C-M-space"): with_or_set_mark(K("C-right")),
+    K("C-s"): K("F3"),
+    K("C-M-s"): K("C-s"),
+    K("C-r"): K("Shift-F3"),
+    K("C-r"): K("C-M-r"),
+    K("M-Shift-key_5"): K("C-h"),
+    K("C-g"): [K("esc"), set_mark(False), K("C")],
+    K("C-M-g"): K("C-g"),
+    K("C-q"): escape_next_key,
+    K("C-M-q"): K("C-q"),
+    K("C-x"): {
+        K("h"): [K("C-home"), K("C-a"), set_mark(True)],
+        K("C-f"): K("C-o"),
+        K("C-s"): K("C-s"),
+        K("k"): K("C-f4"),
+        K("C-c"): K("C-q"),
+        K("C-g"): pass_through_key,
+        K("u"): [K("C-z"), set_mark(False)],
+    },
+    K("C-M-s"): K("C-s")
+}, "Emacs-like keys")
+
+#### END GLOBAL EMACS
 
 
 #### BEGIN GLOBAL
