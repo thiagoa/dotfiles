7b4be47fac8c656e82515c8656f82917f15269a3
Thiago A. Silva <thiagoaraujos@gmail.com>
2020-07-09 15:01:30 -0300

    Add aliases to activate Gnome connections from the command line

--- config/zshaliases.sh
+++ config/zshaliases.sh
@@ -1,3 +1,7 @@
+alias activate-vpn="nmcli con up id"
+alias deactivate-vpn="nmcli con down id"
+alias kill-vpn="sudo pkill openvpn"
+
 if [[ "$(uname -s)" == "Linux" ]]; then
   alias xclip="xclip -selection c"
   alias open="xdg-open"
