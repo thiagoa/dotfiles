71abb4337d78b3858eeb6c1c31be6f3a0da9adea
Thiago A. Silva <thiagoaraujos@gmail.com>
2021-04-08 16:02:39 -0300

    Update autokey patch

--- linux/autokey/setup.sh
+++ linux/autokey/setup.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 autokey_config_dir="$HOME/.config/autokey/data"
-autokey_model="/home/thiago/.local/lib/python3.7/site-packages/autokey/model.py"
+pip_autokey_model="/home/thiago/.local/lib/python3.7/site-packages/autokey/model.py"
+package_autokey_model="/usr/lib/python3/dist-packages/autokey/model.py"
 
-if [[ -f "$autokey_model" ]]; then
+if [[ -f "$package_autokey_model" ]]; then
   old_code="return bool(r.match(window_info.wm_title)) or bool(r.match(window_info.wm_class))"
   new_code="return bool(r.match(window_info.wm_class))"
 
-  sed -i "s/$old_code/$new_code/g" $autokey_model
+  sed -i "s/$old_code/$new_code/g" $package_autokey_model
 
   echo 'Autokey has been patched'
 else
