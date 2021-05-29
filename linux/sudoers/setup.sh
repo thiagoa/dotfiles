cc17b68a9cc7becccf131d890b865ec519ae646e
Thiago A. Silva <thiagoaraujos@gmail.com>
2021-05-29 13:08:18 -0300

    Automatically setup sudoers

---
+++ linux/sudoers/setup.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+sudoers_file="/etc/sudoers"
+perm_prefix="thiago ALL=NOPASSWD:"
+perms="$perm_prefix /usr/bin/openvpn*, /home/thiago/bin/xkeysnail*, /usr/local/bin/xkeysnail*, /usr/bin/xhost, /usr/bin/apt*, /usr/bin/apt-get*, /usr/sbin/pm-suspend"
+
+# Remove permission line
+sudo cat "$sudoers_file" | grep -v "$perm_prefix" | sudo tee -a "$sudoers_file" > /dev/null
+
+# Insert the most updated one again
+echo "$perms" | sudo tee -a "$sudoers_file" > /dev/null
