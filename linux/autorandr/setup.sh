9f3ef896469de6f4579617ec4c0108c2152986da
Thiago A. Silva <thiagoaraujos@gmail.com>
2021-06-26 17:00:03 -0300

    Install autorandr hooks

--- linux/autorandr/setup.sh
+++ linux/autorandr/setup.sh
@@ -2,3 +2,8 @@
 
 rm -rf $HOME/.config/autorandr
 ln -snf $HOME/.dotfiles/linux/autorandr/config $HOME/.config/autorandr
+
+hooks_dir="/etc/xdr/autorandr"
+
+sudo mkdir -p $hooks_dir 2> /dev/null
+sudo cp $HOME/.dotfiles/linux/autorandr/hooks/postswitch $hooks_dir
