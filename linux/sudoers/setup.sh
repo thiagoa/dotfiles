9e68c2e1d466ac7cb5e188d000a157116d157a96
Thiago A. Silva <thiagoaraujos@gmail.com>
2021-11-12 22:29:23 -0300

    Temporary solution to reapply sysctl configs on startup

--- linux/sudoers/setup.sh
+++ linux/sudoers/setup.sh
@@ -2,7 +2,7 @@
 
 sudoers_file="/etc/sudoers"
 perm_prefix="thiago ALL=NOPASSWD:"
-perms="$perm_prefix /usr/bin/openvpn*, /home/thiago/bin/xkeysnail*, /usr/local/bin/xkeysnail*, /usr/bin/xhost, /usr/bin/apt*, /usr/bin/apt-get*, /usr/sbin/pm-suspend"
+perms="$perm_prefix /usr/bin/openvpn*, /home/thiago/bin/xkeysnail*, /usr/local/bin/xkeysnail*, /usr/bin/xhost, /usr/bin/apt*, /usr/bin/apt-get*, /usr/sbin/pm-suspend, /home/thiago/bin/restart-sysctl"
 
 # Remove permission line
 sudo cat "$sudoers_file" | grep -v "$perm_prefix" | sudo tee -a "$sudoers_file" > /dev/null
