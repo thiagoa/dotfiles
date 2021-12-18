5ca7eae85956d31e801d4a84d29584de19f79c91
Thiago A. Silva <thiagoaraujos@gmail.com>
2021-12-18 19:38:36 -0300

    Change spotify installation method to deb

--- linux/packages/setup_graphical_packages.sh
+++ linux/packages/setup_graphical_packages.sh
@@ -87,13 +87,19 @@ sudo apt-get install \
 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
+# Spotify
+
+curl -sS https://download.spotify.com/debian/pubkey_5E3C45D7B312C643.gpg | sudo apt-key add -
+echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
+
+sudo apt install spotify-client
+
 # Indicator sticky notes
 
 sudo add-apt-repository ppa:umang/indicator-stickynotes -y && sudo apt-get update && sudo apt-get install indicator-stickynotes -y
 
 # Install flatpaks
 
-flatpak install flathub com.spotify.Client
 flatpak install flathub org.gabmus.hydrapaper
 flatpak install flathub uk.co.ibboard.cawbird
 flatpak install flathub com.discordapp.Discord
