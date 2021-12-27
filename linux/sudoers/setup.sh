ad391a75526da6de65ee4da68e30cc69d74d8279
Thiago A. Silva <thiagoaraujos@gmail.com>
2021-12-27 18:25:02 -0300

    Fix severe bug where sudoers would keep growing and accumulating

--- linux/sudoers/setup.sh
+++ linux/sudoers/setup.sh
@@ -5,7 +5,8 @@ perm_prefix="thiago ALL=NOPASSWD:"
 perms="$perm_prefix /usr/bin/openvpn*, /home/thiago/bin/xkeysnail*, /usr/local/bin/xkeysnail*, /usr/bin/xhost, /usr/bin/apt*, /usr/bin/apt-get*, /usr/sbin/pm-suspend, /home/thiago/bin/restart-sysctl"
 
 # Remove permission line
-sudo cat "$sudoers_file" | grep -v "$perm_prefix" | sudo tee -a "$sudoers_file" > /dev/null
+sudo cat "$sudoers_file" | grep -v "$perm_prefix" | sudo tee "$sudoers_file.tmp" > /dev/null
+sudo mv "$sudoers_file.tmp" "$sudoers_file"
 
-# Insert the most updated one again
-echo "$perms" | sudo tee -a "$sudoers_file" > /dev/null
+# Reinsert the most updated permission line
+sudo echo "$perms" | sudo tee -a "$sudoers_file" > /dev/null
