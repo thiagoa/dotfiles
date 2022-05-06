4fd6e2de50995452a43b327cafc4d51dc83bf76b
Thiago A. Silva <thiagoaraujos@gmail.com>
2022-05-06 17:53:29 -0300

    Include mdl markdown configuration

---
+++ markdown_lint_styles.rb
@@ -0,0 +1,22 @@
+# https://github.com/markdownlint/markdownlint/blob/master/lib/mdl/rules.rb
+
+rule "header-increment"
+
+rule "header-style",
+  style: :atx
+
+rule "no-multiple-blanks"
+
+rule "blanks-around-headers"
+
+rule "header-start-left"
+
+rule "no-duplicate-header"
+
+rule "blanks-around-fences"
+
+rule "blanks-around-lists"
+
+rule "no-emphasis-as-header"
+
+rule "no-space-in-links"
