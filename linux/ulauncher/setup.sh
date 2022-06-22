a840c7f27cc2762c44fd0d117cf85b13267836d3
Thiago A. Silva <thiagoaraujos@gmail.com>
2022-06-22 12:39:34 -0300

    Now that I use fractional scaling I do not need to tweak ulauncher fonts

--- linux/ulauncher/setup.sh
+++ linux/ulauncher/setup.sh
@@ -2,34 +2,7 @@
 
 dir="$(cd "$(dirname "$0")" && pwd)"
 backup_ui_dir="$dir/ui"
-ulauncher_ui_dir="/usr/share/ulauncher/ui"
 
 if [[ -d "$HOME/Dropbox/Linux/ulauncher-config" ]]; then
   ln -sfn $HOME/Dropbox/Linux/ulauncher-config $HOME/.config/ulauncher
 fi
-
-if [[ ! -d "$ulauncher_ui_dir" ]]; then
-  exit
-fi
-
-if [[ "$1" == "--restore" ]]; then
-  sudo apt-get --reinstall install ulauncher
-  echo
-  echo "Ulauncher Successfully restored"
-  exit
-fi
-
-for path in `find $backup_ui_dir -type f -name *.ui`; do
-  file=`basename $path`
-  sudo cp $path $ulauncher_ui_dir/$file
-done
-
-echo "Ulauncher files copied successfully"
-
-# if pgrep ulauncher > /dev/null; then
-#   echo 'Restarting Ulauncher...'
-#   killall ulauncher
-#   sleep 2
-#   ulauncher > /dev/null &
-#   disown %1
-# fi
