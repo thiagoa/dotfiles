738ccbb309959285caceaf11bd1eaac929f9fc6c
Thiago A. Silva <thiagoaraujos@gmail.com>
2022-12-27 17:31:33 -0300

    Update udev setup to tag trackballs as trackballs, not mouses

--- linux/udev/setup.sh
+++ linux/udev/setup.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
-echo 'Linking udev rule scripts...'
+echo 'Linking udev rule and hwdb scripts...'
 
 dir="$(cd "$(dirname "$0")" && pwd)"
 rules_dir="/etc/udev/rules.d"
+hwdb_dir="/etc/udev/hwdb.d"
 
 if [[ -d "$rules_dir" ]]; then
-  sudo cp -f ~/.dotfiles/linux/udev/* $rules_dir
+  sudo cp -f ~/.dotfiles/linux/udev/rules/* $rules_dir
   sudo udevadm control --reload
 else
-  echo "No udev found!"
+  echo "No udev rules dir found!"
+fi
+
+if [[ -d "$hwdb_dir" ]]; then
+  sudo cp -f ~/.dotfiles/linux/udev/hwdb/* $hwdb_dir
+  sudo systemd-hwdb update
+else
+  echo "No udev rules dir found!"
 fi
